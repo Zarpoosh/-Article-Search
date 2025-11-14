40acfa2e482ea1a92617afccbbc49c2b247c31af
Zarpoosh <mzar67839@gmail.com>
2025-11-14 12:41:38 +0330

    change icon

--- app/components/QRCodeGenerator/QRCodeGenerator.tsx
+++ app/components/QRCodeGenerator/QRCodeGenerator.tsx
@@ -10,8 +10,8 @@ interface QRCodeGeneratorProps {
 
 export default function QRCodeGenerator({
   articleLink,
-  // articleTitle,
-}: QRCodeGeneratorProps) {
+}: // articleTitle,
+QRCodeGeneratorProps) {
   const [showQR, setShowQR] = useState(true); // تغییر: پیش‌فرض نمایش QR Code
 
   // const downloadQRCode = () => {
@@ -57,13 +57,13 @@ export default function QRCodeGenerator({
           </button> */}
         </div>
       )}
-<a
-  href={articleLink}
-  target="_blank"
-  rel="noopener noreferrer"
-  className="text-fuchsia-500 flex items-center justify-center gap-2 hover:text-fuchsia-700 break-all"
->
-  <svg
+      <a
+        href={articleLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-fuchsia-500 flex items-center justify-center gap-2 hover:text-fuchsia-700 break-all"
+      >
+        {/* <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
@@ -76,11 +76,23 @@ export default function QRCodeGenerator({
       strokeLinejoin="round"
       d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
     />
-  </svg>
-  articleLink
-</a>
+  </svg> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+          />
+        </svg>
+        articleLink
+      </a>
     </div>
   );
 }
-
-
